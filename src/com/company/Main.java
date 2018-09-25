10d295c29ad1b6cd32f94a70bdcacd10fa63ec43
thanosfromfortnite <43213177+thanosfromfortnite@users.noreply.github.com>
2018-09-25 11:26:07 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("What's your name?");
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         System.out.println("Hello, " + name);
 
-        System.out.println("CHOOSE YOUR DIFFICULTY LEVEL. (1/2/3/4)");
+        System.out.println("Choose your difficulty level. (1/2/3/4)");
         input = new Scanner(System.in);
         int difficulty = Integer.parseInt(input.nextLine());
         int x = (int) (Math.pow(10.0, (float) (difficulty)) * Math.random());
         int y = 0;
-        System.out.println("Now you must guess the correct number.");
-        System.out.println(x);
+        System.out.println("Now you must guess the correct number. Good luck, " + name);
+        int answer = 0;
+        while (answer != x) {
+            y++;
+            input = new Scanner(System.in);
+            answer = Integer.parseInt(input.nextLine());
+            if (answer != x) {
+                System.out.println("That's not right. Try harder, " + name);
+            }
+
+        }
+        if (y == 1) {
+            System.out.println("Congratulations, " + name + "! You guessed it right on the first try!");
+        } else {
+            System.out.println("Congratulations, " + name + ". It only took you " + y + " tries to do it.");
+        }
     }
 }
