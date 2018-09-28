3ae719ba3db56ea91e944418a4826c50ca07ebc9
thanosfromfortnite <43213177+thanosfromfortnite@users.noreply.github.com>
2018-09-28 11:20:20 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -70,12 +70,13 @@ public class Main {
             input = new Scanner(System.in);
             difficulty = Integer.parseInt(input.nextLine());
         }
-        int maxRange = (int) Math.pow(10, difficulty); int minRange = 1; int botAnswer = (maxRange + minRange) / 2; String answer;
+        int maxRange = ((int) Math.pow(10, difficulty)) + 1; int minRange = 0; int botAnswer = (maxRange + minRange) / 2; String answer; int y = 0;
 
-        System.out.println("Please think deeply of a number between 1 and " + maxRange + ".");
+        System.out.println("Please think deeply of a number between 1 and " + (maxRange - 1) + ".");
         boolean correct = false;
+        String output = "I have discovered your number, " + botAnswer + ". It has taken me " + y + " tries to do it.";
         while (!correct) {
-            System.out.println("Is the number (less) than , (greater) than, or (equal) to " + (maxRange+minRange)/2 + "?");
+            System.out.println("Is the number (less) than, (greater) than, or (equal) to " + (maxRange+minRange)/2 + "?");
             input = new Scanner(System.in);
             answer = input.nextLine();
             while (!answer.equals("greater") && !answer.equals("less") && !answer.equals("equal")) {
@@ -90,10 +91,15 @@ public class Main {
             }
             else correct = true;
             botAnswer = (maxRange + minRange) / 2;
+            y ++;
+            if (y == 1) {
+                output = "I have found your number, " + botAnswer + " in just one try!";
+            }
             if (botAnswer == maxRange || botAnswer == minRange) {
                 correct = true;
+                output = "ERR: User has lied on one of the prompts or number does not exist.";
             }
         }
-        System.out.println("I have discovered your number, " + botAnswer);
+        System.out.println(output);
     }
 }
